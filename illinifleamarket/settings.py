d639bf0d40be034eeade659c311b94b621d6bb89
may0723 <maywu0723@gmail.com>
2018-12-01 15:02:30 -0600

    local settings

--- illinifleamarket/settings.py
+++ illinifleamarket/settings.py
@@ -156,3 +156,8 @@ STATICFILES_DIRS = [
 # Media Folder settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
